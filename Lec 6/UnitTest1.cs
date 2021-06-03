100f43f5af636c82e9b573915ddb7a606cbf504d
Alexandr <alexraiko2211@gmail.com>
2021-06-03 17:43:15 +0300

    Edit

--- Lec 6/UnitTest1.cs
+++ Lec 6/UnitTest1.cs
@@ -3,10 +3,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 namespace Lec_6
 {
@@ -27,15 +29,16 @@ namespace Lec_6
         {
             string url = "https://demoqa.com/automation-practice-form";
             driver.Navigate().GoToUrl(url);
+            var Subject = new[] { "Math","Chemistry", "Physics" };
             var userIds = 0;
             var testUsers = new Faker<User>()
+                .RuleFor(o => o.Subject, f => f.PickRandom(Subject))
                 .RuleFor(o => o.Phone, f => f.Person.Phone)
                 .RuleFor(o => o.FirstName, f => f.Name.FirstName())
                 .RuleFor(o => o.LastName, f => f.Name.LastName())
                 .RuleFor(o => o.Adress, f => f.Lorem.Sentence())
                 .RuleFor(o => o.EmailAddress, (f, u) => f.Internet.Email(u.FirstName, u.LastName));
             var user = testUsers.Generate();
-
 
             IWebElement FirstnameField = driver.FindElement(By.CssSelector("input[id='firstName']"));
             FirstnameField.Click();
@@ -64,6 +67,16 @@ namespace Lec_6
             Checkbox.ElementAt(new Random().Next(0, Checkbox.Count - 1)).Click();
 
 
+            /*IWebElement SubjectField = driver.FindElement(By.CssSelector("input[id='subjectsInput']"));
+            SubjectField.Click();
+            SubjectField.Clear();
+            SubjectField.SendKeys(user.Subject);
+            Thread.Sleep(2000);
+            IWebElement SubjectBtn = driver.FindElement(By.XPath("//*[contains(text(),'" + user.Subject + "')]"));
+            SubjectBtn.Click();
+*/
+            IWebElement SubmitBtn = driver.FindElement(By.CssSelector("#userForm"));
+            SubmitBtn.Submit();
         }
 
         [TearDown]
@@ -81,12 +94,5 @@ namespace Lec_6
         public string Adress { get; set; }
         public string Subject { get; set; }
 
-    }
-
-    public enum Subject
-    {
-        Math,
-        Chemistry,
-        Physics
     }
 }
